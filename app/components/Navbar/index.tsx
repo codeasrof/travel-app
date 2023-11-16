7c63b5fb817a57d1b38782c8d3794d08bfbed610
codeasrof <vitalidade.08-devorador@icloud.com>
2023-11-16 17:22:00 -0300

    Navbar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,5 +1,7 @@
+import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import Button from "../Button";
 
 export default function Navbar() {
   return (
@@ -8,6 +10,36 @@ export default function Navbar() {
             <Link href="/">
                 <Image src="/hilink-logo.svg" alt="Hilink Logo" width={74} height={29}/> 
             </Link>
+
+            <ul className="hidden h-full gap-12 lg:flex">
+                {NAV_LINKS.map((link) => (
+                    <Link 
+                        href={link.href} 
+                        key={link.key}
+                        className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold hover:text-[#2EB05B]"
+                    >
+                        {link.label}
+                    </Link>
+                ))}
+            </ul>
+
+            <div className="lg:flexCenter hidden">
+                <Button
+                    type="button"
+                    title="Login"
+                    icon="/user.svg"
+                    variant="btn_dark_green"
+                />
+            </div>
+
+            <Image 
+                src="menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="inline-block cursor-pointer lg:hidden"
+            />
+
         </nav>
     </header>
   )
